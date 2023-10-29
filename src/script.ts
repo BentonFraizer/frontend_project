6a0fac20ad0c785ea46fa67173d74e671918b4f7
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2023-10-29 14:04:47 +0300

    build(project): added settings for work with html, css, js and ts files

---
+++ src/script.ts
@@ -0,0 +1,6 @@
+import './styles.css';
+
+const varForTest = 'very important value';
+const newVar = `${varForTest}!`;
+
+console.log('In this variable we can see: ', newVar);
---
+++ webpack.config.cjs
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLInlineCSSWebpackPlugin = require('html-inline-css-webpack-plugin').default;
+
+module.exports = {
+  entry: './src/script.ts',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './src/index.html',
+    }),
+    new HTMLInlineCSSWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};

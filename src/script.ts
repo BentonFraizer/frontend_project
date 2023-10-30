02a4cae38ac241d776749226f1c2c0b574f5f308
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2023-10-30 22:27:34 +0300

    test

--- src/script.ts
+++ src/script.ts
@@ -4,6 +4,8 @@ const varForTest = 'very important value';
 const newVar = `${varForTest}!`;
 const fiveNum = 5; // переменная объявлена для проверки работоспособности ESLint
 const doubleNumber = (someNum) => someNum * 2;
+
+doubleNumber(fiveNum);
 doubleNumber(fiveNum);
 
 console.log('In this variable we can see:', newVar);

5ae5482b876abbc4e46b280f0680566e6d16f11a
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2023-10-30 22:07:26 +0300

    fix. Changed name of file prettier.json with .prettierrc

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,9 @@
-import "./styles.css";
+import './styles.css';
 
-const varForTest = "very important value";
+const varForTest = 'very important value';
 const newVar = `${varForTest}!`;
 const fiveNum = 5; // переменная объявлена для проверки работоспособности ESLint
 const doubleNumber = (someNum) => someNum * 2;
 doubleNumber(fiveNum);
 
-console.log("In this variable we can see:", newVar);
+console.log('In this variable we can see:', newVar);

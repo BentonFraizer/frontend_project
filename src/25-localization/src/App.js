0cdc75c809cbc9969d6e730d314dc61d628614b0
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-01-29 22:07:28 +0300

    Добавляет префикс item- в вызов функции uniqueId

--- src/25-localization/src/App.js
+++ src/25-localization/src/App.js
@@ -2,31 +2,33 @@ import './App.css';
 import uniqueId from 'lodash/uniqueId';
 
 function App() {
-  const firstUniqueId = uniqueId();
-  const secondUniqueId = uniqueId();
+  const firstUniqueId = uniqueId('item-');
+  const secondUniqueId = uniqueId('item-');
 
   return (
     <div className="app">
       <header className="header">
         <div className="items">
-          <div className={`item-${firstUniqueId}`}>
-            <div className={`item-${firstUniqueId}__img`}>
+          <div className={`${firstUniqueId}`}>
+            <div className={`${firstUniqueId}__img`}>
               <img src="https://source.unsplash.com/random/200x200" alt="random view" />
             </div>
-            <p className={`item-${firstUniqueId}__description`}>Это описание первого товара</p>
-            <a className={`item-${firstUniqueId}__link`} href="https://google.com" target="_blank" rel="noopener noreferrer">
+            <p className={`${firstUniqueId}__description`}>Это описание первого товара</p>
+            <a className={`${firstUniqueId}__link`} href="https://google.com" target="_blank" rel="noopener noreferrer">
               Подробнее
             </a>
+            <button className={`${firstUniqueId}__btn`}>Купить</button>
           </div>
 
-          <div className={`item-${secondUniqueId}`}>
-            <div className={`item-${secondUniqueId}__img`}>
+          <div className={`${secondUniqueId}`}>
+            <div className={`${secondUniqueId}__img`}>
               <img src="https://source.unsplash.com/random/200x200" alt="random view" />
             </div>
-            <p className={`item-${secondUniqueId}__description`}>Это описание второго товара</p>
-            <a className={`item-${secondUniqueId}__link`} href="https://google.com" target="_blank" rel="noopener noreferrer">
+            <p className={`${secondUniqueId}__description`}>Это описание второго товара</p>
+            <a className={`${secondUniqueId}__link`} href="https://google.com" target="_blank" rel="noopener noreferrer">
               Подробнее
             </a>
+            <button className={`${firstUniqueId}__btn`}>Купить</button>
           </div>
         </div>
       </header>

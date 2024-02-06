228e1dbfcc3c28bee746ac2baaf6a79f0eee949a
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-02-06 22:30:54 +0300

    Compelted home assignment 29-common-modules

---
+++ src/29-common-modules/src/createDistDir.js
@@ -0,0 +1,33 @@
+import { access, mkdir } from 'node:fs/promises';
+import path from 'node:path';
+import url from 'node:url';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const makeDir = async () => {
+  const dirPath = path.resolve(__dirname, '../dist');
+  try {
+    await access(dirPath);
+    console.log('directory "dist" already exists');
+  } catch (e) {
+    await mkdir(dirPath);
+    console.log(`created ${dirPath}`);
+  }
+};
+
+makeDir();
+
+// Моя первая реализация до просмотра примера из лекции
+
+// try {
+//   const distFolder = new URL('../dist', import.meta.url);
+//   const createDir = await mkdir(distFolder, { recursive: true });
+
+//   if (createDir !== undefined) {
+//     console.log(`created ${createDir}`);
+//   } else {
+//     console.log('directory "dist" already exists');
+//   }
+// } catch (err) {
+//   console.error(err.message);
+// }
